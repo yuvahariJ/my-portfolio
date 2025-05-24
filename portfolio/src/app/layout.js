a30a27b372054dccd3b7338b579275277b324f12
Yuvahari JR <yuvahari.j@gmail.com>
2025-05-24 12:00:30 +0530

    Deploy changes

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,19 +1,9 @@
 "use client"
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider, useTheme } from "@/Context/ThemeProvider/Theme";
 import Header from "@/Pages/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 function ThemedApp({ children }) {
   const { theme } = useTheme();
