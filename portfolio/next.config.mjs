8a31726373c1f1b21efe65ad1223e28d28dc78bb
Yuvahari JR <yuvahari.j@gmail.com>
2025-05-24 11:27:53 +0530

    Deploy chnages

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/my-portfolio',  // repo name
+  assetPrefix: '/my-portfolio',
+};
 
 export default nextConfig;
